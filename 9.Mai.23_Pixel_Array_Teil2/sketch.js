721523eaea4eb6464166860b3dadbf063a77d6b8
vanessarenggli <124628600+vanessarenggli@users.noreply.github.com>
2023-05-10 16:32:08 +0200

    Add files via upload

---
+++ 9.Mai.23_Pixel_Array_Teil2/sketch.js
@@ -0,0 +1,30 @@
+let img;
+let pixels;
+
+function setup() {
+    createCanvas(500, 600);
+    img = loadImage("asset/cindy-small.jpg");
+    pixelDensity(1);
+}
+
+function draw() {
+    background(220);
+    image(img, 0, 0);
+
+    img.loadPixels();
+    //     let pix = img.pixels[23];
+    //     let b = brightness(pix);
+    //     console.log(b);
+
+
+    for (let i = 0; i < img.pixels.length; i = i + 4) { // +4 weil ein Pixel immer aus 4 Werten besteht, rot, grün und blauwert und die Helligkeit 
+
+        // img.pixels[i] = random(255);
+        img.pixels[i] = 255
+
+    }
+
+    img.updatePixels();
+
+    image(img, 0, 0);
+}
